a149f684a21e3c7e55a6b67cc3c305e547e062c5
LastAeon <syihabuddiny@gmail.com>
2022-06-10 08:10:21 +0700

    feat: alternative use sqlite

--- database.py
+++ database.py
@@ -8,7 +8,8 @@ config = configparser.ConfigParser()
 config.read(".dbconfig")
 config = config['DB']
 
-SQLALCHEMY_DATABASE_URL = "{}://{}:{}@{}/{}".format(config['type'],config['username'],config['password'],config['server'],config['name'])
+# SQLALCHEMY_DATABASE_URL = "{}://{}:{}@{}/{}".format(config['type'],config['username'],config['password'],config['server'],config['name'])
+SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
